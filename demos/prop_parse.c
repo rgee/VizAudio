e9e4b77ad00a2c95017d817a7019c8b274e3e883
Ryan <rgee@wesleyan.edu>
2009-06-30 15:15:29 -0400

    Added demo for parsing property lists

---
+++ demos/prop_parse.c
@@ -0,0 +1,39 @@
+#include "canberra.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+    ca_context *c;
+    ca_proplist *p;
+    int ret;
+    
+    char *out;
+
+    ret = ca_context_create(&c);
+    fprintf(stderr, "create: %s\n", ca_strerror(ret));
+    
+    ret = ca_context_change_props(c,
+                                    CA_PROP_APPLICATION_NAME, "Example Application",
+                                    CA_PROP_APPLICATION_ID,     "org.ryan.test",
+                                    CA_PROP_WINDOW_X11_SCREEN, getenv("DISPLAY"),
+                                    NULL);
+
+    ret = ca_context_open(c);
+    fprintf(stderr, "open: %s\n", ca_strerror(ret));
+    
+    ca_proplist_create(&p);
+    ca_proplist_sets(p, CA_PROP_EVENT_ID, "desktop-login");
+    ca_proplist_sets(p, CA_PROP_MEDIA_FILENAME, "/usr/share/sounds/uxknkurz.wav");
+    ca_proplist_sets(p, CA_PROP_MEDIA_NAME, "New email recieved");
+    
+    out = ca_proplist_gets_unlocked(p, CA_PROP_WINDOW_ID);
+    
+    printf("%s",out);
+    
+    printf("\n");
+    
+    return 0;
+}
