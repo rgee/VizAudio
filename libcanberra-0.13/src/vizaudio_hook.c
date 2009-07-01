fd631cd318b204ed4c448d07a18239e69cfcc0ad
fnv <fnv@ubuntu.ubuntu-domain>
2009-07-01 10:21:22 -0400

    fixed some bugs

--- libcanberra-0.13/src/vizaudio_hook.c
+++ libcanberra-0.13/src/vizaudio_hook.c
@@ -1,4 +1,5 @@
 #include "canberra.h"
+#include "proplist.h"
 #include "vizaudio_hook.h"
 #include "vizaudio.h"
 
@@ -10,7 +11,7 @@ void vizaudio_display(ca_proplist *data) {
 
     char *out;
 
-    out = (char*) ca_proplist_gets_unlocked(p, CA_PROP_EVENT_ID);
+    out = (char*) ca_proplist_gets_unlocked(data, CA_PROP_EVENT_ID);
 
     flash_text("ID: %s", out);
 
