24690475e4fbcf261c4d1c6897f8ea087ce9e67f
Ryan <rgee@wesleyan.edu>
2009-07-06 13:39:38 -0400

    Added Gconf support. vizaudio.c now checks to see if the appropriate Gconf flag exists. If not, it will create it and default to activated. (easily changable).

--- src/vizaudio.h
+++ src/vizaudio.h
@@ -13,3 +13,6 @@ static gboolean endFlash(GtkWidget *window);
 static void screen_changed(GtkWidget *widget, GdkScreen *old_screen, gpointer user_data);
 static gboolean time_handler (GtkWidget *widget);
 static gboolean textDisplay(GtkWidget *widget, GdkEventExpose *event, gpointer user_data);
+
+int isVAEnabled();
+void initGConfFlag();
